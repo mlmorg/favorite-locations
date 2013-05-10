6df9f2afc806ccac8a1bfc933568f18fa85e0c03
Matt Morgan <mlmorg@gmail.com>
2013-05-09 21:03:25 -0400

    Don't send unnecessary attrs when saving model

--- app/assets/javascripts/locations/model.js
+++ app/assets/javascripts/locations/model.js
@@ -2,7 +2,12 @@ define(function (require, exports, module) {
 
   module.exports = Backbone.Model.extend({
     urlRoot: '/locations',
-    idAttribute: '_id'
+    idAttribute: '_id',
+
+    toJSON: function () {
+      var obj = Backbone.Model.prototype.toJSON.call(this);
+      return _.omit(obj, ['_id', '__v']);
+    }
   });
 
 });
